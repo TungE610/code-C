c3a0c166b862f526487b15fbbfb8c8fdd8e68304
TungE610 <81414026+TungE610@users.noreply.github.com>
2021-04-05 08:43:36 +0700

    Update tuan2.c

--- tuan2.c
+++ tuan2.c
@@ -64,6 +64,18 @@ int main(){
         {
             printf(" %0.2f ", a[i]);
         }
-    return 0;
-    /*Bài toán Tháp Hà Nội*/
+    /*Bài toán tim max cua 3 so*/
+    float x,y,z,max;
+    printf("Nhap 3 so x,y,z");
+    scanf("%f%f%f",&x,&y,&z);
+    max=x;
+    if(y>max){
+        max=y;
+    }
+    if(z>max){
+        max=z;
+    }
+    printf("Gia tri lon nhat cua 3 so la %f",max);
+    
 }
+ return 0;
