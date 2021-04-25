9caf4748dc2f4ea06d187c8f83091025b20ee89d
TungE610 <81414026+TungE610@users.noreply.github.com>
2021-04-25 22:15:39 +0700

    Add files via upload

---
+++ baitapweek4.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<limits.h>
+#include<math.h>
+int main(){
+	printf("bai 4.3");
+	char a[100];
+	printf("\nLimit of the data types int : %d -> %d \n",INT_MIN,INT_MAX);
+	printf("Limit of the data types long : %d -> %d \n",LONG_MIN,LONG_MAX);
+	printf("Limit of the data types char : %d -> %d \n",CHAR_MIN,CHAR_MAX);
+	printf("Enter the String: ");
+	scanf("%s",a);
+	printf("The String : %s\n",a);
+	/*4.5*/
+	printf("Bai 4.5");
+	const float Pi=3.14;
+	float x;
+	printf("\nenter the radius of a circle:  ");
+	scanf("%f",&x);
+	printf("Its area : %5.2f",pow(x,2)*Pi);
+	/*4.7*/
+	printf("\nBai 4.7");
+	int y,p,q;
+	char z[100];
+	char t[100];
+ printf("\nEnter the Qty: ");
+ scanf("%d",&y);
+ printf("\nEnter the ISBN: ");
+ scanf("%s",&z);
+  printf("Enter the Title: ");
+scanf("%s",&t);
+  printf("\nEnter the Price($): ");
+scanf("%d",&p);
+  printf("\nEnter the Price($): ");
+
+printf("\nEnter the Quantity");
+scanf("%d",)&q
+Printf("\nBK Bookseller\n");
+Printf("Qty|ISBN|Title|Price|Total");
+printf("%c",)
+printf()
+return 0;
+}
