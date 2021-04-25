e19c2750003cd8f7a62f648b7d7e34a2a0e45c67
TungE610 <81414026+TungE610@users.noreply.github.com>
2021-04-25 22:08:41 +0700

    Add files via upload

---
+++ baitapweek3.c
@@ -0,0 +1,55 @@
+//
+//  main.c
+//  week_3c
+//
+//  Created by bui danh tung on 12/04/2021.
+//
+
+#include<stdio.h>
+int main(){
+    /*bai 3.1*/
+    printf("\nKich co cua kieu giu lieu Int la %d",sizeof(int));
+    printf("\nKich co cua kieu giu lieu long la %d",sizeof(long));
+    printf("\nKich co cua kieu giu lieu short la %d",sizeof(short));
+    printf("\nKich co cua kieu giu lieu double la %d",sizeof(double));
+    printf("\nKich co cua kieu giu lieu char la %d",sizeof(char));
+
+    /*bai 3.2*/
+    int year;
+    float height;
+    year=21;
+    height=1.77;
+    printf("\nAli is %d years old and %5.2f meter height\n",year,height);
+    /*bai 3.3*/
+
+    char word[20];
+    printf("What's your name ?\n");
+    scanf("%19s",word);
+    printf("Welcome to C programing");
+    /*Bai 3.4*/
+    int n,m;
+    printf("\nEnter first integer number:");
+  scanf("%d",&n);
+     printf("\nEnter second integer number");
+ scanf("%d",&m);
+ printf("sum of 2 numbers:%d",m+n);
+    /*Bai 3.5*/
+ int x,y,z,t;
+ printf("\nenter the day of the week:  ");
+ scanf("%d",&x);
+  printf("\n the day of the week:%d",x);
+printf("\nenter the number of overnight: ");
+  scanf("%d",&y);
+   printf("\nthe number of overnight:%d",y);
+printf("\nenter the number of three-day: ");
+   scanf("%d",&z);
+    printf("\nthe number of three-day:%d ",z);
+printf("\nenter the number of weekly: ");
+  scanf("%d",&t);
+   printf("\nthe number of weekly:%d",t);
+   printf("\ntotal cost of renting the DVDs:%d",7*y+5*z+3*t);
+
+
+
+ return 0;
+}
