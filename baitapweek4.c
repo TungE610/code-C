e0b6f9fddcaeb95c1e02c5f1daab87167fe66885
TungE610 <81414026+TungE610@users.noreply.github.com>
2021-05-08 23:22:53 +0700

    Update baitapweek4.c

--- baitapweek4.c
+++ baitapweek4.c
@@ -2,26 +2,42 @@
 #include<limits.h>
 #include<math.h>
 int main(){
-	printf("bai 4.3");
-	char a[100];
-	printf("\nLimit of the data types int : %d -> %d \n",INT_MIN,INT_MAX);
-	printf("Limit of the data types long : %d -> %d \n",LONG_MIN,LONG_MAX);
-	printf("Limit of the data types char : %d -> %d \n",CHAR_MIN,CHAR_MAX);
-	printf("Enter the String: ");
-	scanf("%s",a);
-	printf("The String : %s\n",a);
-	/*4.5*/
-	printf("Bai 4.5");
-	const float Pi=3.14;
-	float x;
-	printf("\nenter the radius of a circle:  ");
-	scanf("%f",&x);
-	printf("Its area : %5.2f",pow(x,2)*Pi);
-	/*4.7*/
-	printf("\nBai 4.7");
-	int y,p,q;
-	char z[100];
-	char t[100];
+    /*Bài 4.1*/
+    int i;
+    float j;
+    printf("enter an integer and an floating point: ");
+    scanf("%d%f",&i,&j);
+    printf("\nthe integer is %d and the floating point is %3.2f\n",i,j);
+    /*Bài 4.2*/
+    char a[100];
+    printf("\nLimit of the data types int : %d -> %d \n",INT_MIN,INT_MAX);
+    printf("Limit of the data types long : %d -> %d \n",LONG_MIN,LONG_MAX);
+    printf("Limit of the data types char : %d -> %d \n",CHAR_MIN,CHAR_MAX);
+    printf("Enter the String: ");
+    scanf("%s",a);
+    printf("The String : %s\n",a);
+    /*Bài 4.3*/
+    char str[20];
+    printf("Enter a string: ");
+    scanf("%[abc]",str);
+    printf("\nthe string is: %s\n",str);
+    
+    /*4.5*/
+    printf("\nBai 4.5");
+    const float Pi=3.14;
+    float x;
+    printf("\nenter the radius of a circle:  ");
+    scanf("%f",&x);
+    printf("Its area : %5.2f",pow(x,2)*Pi);
+    /*Bài 4.6*/
+    int hour;
+    printf("\nEnter the number of hours: ");
+    scanf("%d",&hour);
+    printf("\nThe total of salary is : %d($)\n",40*25000+(hour-40)*40000);
+    /*4.7*/
+    int y,p,q;
+    char z[100];
+    char t[100];
  printf("\nEnter the Qty: ");
  scanf("%d",&y);
  printf("\nEnter the ISBN: ");
@@ -33,10 +49,8 @@ scanf("%d",&p);
   printf("\nEnter the Price($): ");
 
 printf("\nEnter the Quantity");
-scanf("%d",)&q
-Printf("\nBK Bookseller\n");
-Printf("Qty|ISBN|Title|Price|Total");
-printf("%c",)
-printf()
+    scanf("%d",&q);
+printf("\nBK Bookseller\n");
+printf("Qty|ISBN|Title|Price|Total");
 return 0;
 }
