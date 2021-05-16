c5f982cfc0b3e06a8c7aca6eea801f2b3617187d
TungE610 <81414026+TungE610@users.noreply.github.com>
2021-05-16 23:50:24 +0700

    Update week5.c

--- week5.c
+++ week5.c
@@ -14,7 +14,7 @@ int main(){
 int main(){
  int x1,x2,x3;
  printf("enter 3 numbers x1 x2 x3: ");
- scanf("%d%d%d",&a,&b,&c);
+ scanf("%d%d%d",&x1,&x2,&x3);
  if(x1-x2==x2-x3){
  printf("x1=x3\n");
   
